e4f138404c2adb2b2d70fc879424ab5687a5209d
xmonikitaax <48810800+xmonikitaax@users.noreply.github.com>
2024-06-27 11:32:11 -0300

    Add files via upload

---
+++ notebook-monicaaraujo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Data Science Tools and Ecosystem
+
+# ## Introdução
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Linguagens de Ciência de Dados
+# 1. Python
+# 2. R
+# 3. Julia
+# 4. SQL
+# 5. Scala
+
+# ## Bibliotecas de Ciência de Dados
+# 
+# 1. Matplotlib
+# 2. Seaborn
+# 3. Scikit-learn
+# 
+
+# ## Ferramentas de Ciência de Dados
+# 
+# | Ferramenta       | Descrição                                 |
+# |------------------|-------------------------------------------|
+# | Jupyter Notebook | Ambiente interativo para programação      |
+# | RStudio          | IDE para R                                |
+# | Apache Hadoop    | Framework para processamento de Big Data  |
+
+# ## Exemplos de Expressões Aritméticas
+# - Adição: \(3 + 2 = 5\)
+# - Subtração: \(7 - 4 = 3\)
+# - Multiplicação: \(6 \times 3 = 18\)
+# - Divisão: \(8 \div 2 = 4\)
+
+# In[7]:
+
+
+# Multiplicação e Adição de Números
+a = 3
+b = 4
+c = 5
+multiplicacao = a * b
+adicao = (a * b) + c
+print(f'Multiplicação: {multiplicacao}')
+print(f'Adição: {adicao}')
+
+
+# In[8]:
+
+
+# Converter Minutos em Horas
+minutos = 200
+horas = minutos / 60
+print(f'{minutos} minutos equivalem a {horas} horas')
+
+
+# ## Objetivos
+# 1. Demonstrar a criação de células markdown e de código.
+# 2. Praticar a formatação de conteúdo em markdown.
+# 3. Executar operações aritméticas básicas.
+# 4. Aprender a converter unidades de tempo.
+
+# ## Autor: Monica Araujo
